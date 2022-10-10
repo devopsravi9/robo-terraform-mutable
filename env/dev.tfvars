15d6b82f4430a95f062a6d252f9acba39d602a3e
Ravi <ravi@gmail.com>
2022-10-10 12:11:45 +0530

    ec2 code

---
+++ env/dev.tfvars
@@ -0,0 +1,5 @@
+ENV = "dev"
+PROJECT = "roboshop"
+
+//VPC
+VPC_CIDR = "10.50.0.0/16"
--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 module "vpc" {
-  source = "github.com/devopsravi9/module-vpc"
+  source               = "github.com/devopsravi9/module-vpc"
+  VPC_CIDR             = var.VPC_CIDR
+  PROJECT              = var.PROJECT
+  ENV                  = var.ENV
 }
